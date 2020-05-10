5493e3de79b48856111e18ac546d6a442643e0f6
GeovanniAlderete <geovanni.alderete.ruano.201@gmail.com>
2020-05-09 23:51:00 -0500

    API JSON FORD

--- controllers/app.js
+++ controllers/app.js
@@ -13,11 +13,16 @@ app.controller('MainCTRL', function($scope, $http) {
         subTitle: 'Corzo Insur México'
     }
 
-    $http.get("https://www.ford.mx/content/ford/mx/es_mx/.vehiclesmenu.data")
-        .then(function(response) {
-            $scope.content = response.data;
-            console.log(response.data);
-        }, function(response) {
-            $scope.content = "Ocurrió un error en la aplicación";
-        });
+    var settings = {
+        "url": "",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Cookie": "ak_bmsc=53DE48B20CB066D53BA1E3510EEFF8B617C1AC660A4000003C58B75EA88E664D~plCdRyhojCCpb7em0/kltyPebzTDNzBXNrbnvY+rWA0XzG/H5HMcjuEV7RuAyN2igPK4ePJYrt+p6L5XuDJfS2KEdS9OyjRKOMt9YP2chbCy9uGyVFYhVLwjyye58Im6YWoh/+PHa6yS+Ecg8WDJPF2TNlBO6zXsg0X7sRNk9WKeyLra5aTq7sX60G0lIUtU61/T+PPtSxuJn+9N+SLhHIoPBz5iyNcZ451WJCetUgRqQ="
+        },
+    };
+
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
 });
