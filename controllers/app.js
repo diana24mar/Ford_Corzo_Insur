82e002b1abac69ef78b37920c657e050885a27f9
GeovanniAlderete <geovanni.alderete.ruano.201@gmail.com>
2020-05-09 23:51:50 -0500

    API REST

--- controllers/app.js
+++ controllers/app.js
@@ -2,6 +2,11 @@
 
 var app = angular.module('myAppNissan', []);
 
+app.config(function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
 app.controller('MainCTRL', function($scope, $http) {
     $scope.head = {};
     $scope.body = {};
@@ -13,16 +18,11 @@ app.controller('MainCTRL', function($scope, $http) {
         subTitle: 'Corzo Insur México'
     }
 
-    var settings = {
-        "url": "",
-        "method": "GET",
-        "timeout": 0,
-        "headers": {
-            "Cookie": "ak_bmsc=53DE48B20CB066D53BA1E3510EEFF8B617C1AC660A4000003C58B75EA88E664D~plCdRyhojCCpb7em0/kltyPebzTDNzBXNrbnvY+rWA0XzG/H5HMcjuEV7RuAyN2igPK4ePJYrt+p6L5XuDJfS2KEdS9OyjRKOMt9YP2chbCy9uGyVFYhVLwjyye58Im6YWoh/+PHa6yS+Ecg8WDJPF2TNlBO6zXsg0X7sRNk9WKeyLra5aTq7sX60G0lIUtU61/T+PPtSxuJn+9N+SLhHIoPBz5iyNcZ451WJCetUgRqQ="
-        },
-    };
-
-    $.ajax(settings).done(function(response) {
-        console.log(response);
-    });
+    $http.get("/controllers/dataAPI.json")
+        .then(function(response) {
+            $scope.content = response.data;
+            console.log(JSON.stringify($scope.content[0]))
+        }, function(response) {
+            $scope.content = "Something went wrong";
+        });
 });
